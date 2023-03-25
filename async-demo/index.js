f1a8d6e084bf28cf8ce3b2ef5bc277775bfe8798
4sudheer4 <sudheeranugoju4@gmail.com>
2023-03-25 09:53:34 -0700

    async tests

---
+++ async-demo/index.js
@@ -0,0 +1,19 @@
+console.log('Before');
+//this is to perform async execution. 
+//async execution means one thread will take care of all the threads and 
+//instead of serving a process and go to the next process only after finishing the first one, 
+//the async will serve all the processes in order and execute the processes with the given wait time
+
+//here 'before' is executed, timeout is scheduled, after is executed.
+//the below processes are scheduled at 2nd and 4th seconds with 2sec time gap. 
+//where in sync, they will be scheduled at 2nd and 6th seconds.
+setTimeout(() => {
+    console.log('Reading a user from database....');
+}, 2000);
+setTimeout(() => {
+    console.log('Reading a user from database....');
+}, 4000);
+
+
+console.log('After');
+
