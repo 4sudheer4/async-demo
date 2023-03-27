d29dc18848be38caf052a57edabac82111603000
4sudheer4 <sudheeranugoju4@gmail.com>
2023-03-27 12:17:31 -0700

    promise events1

--- async-demo/promises.js
+++ async-demo/promises.js
@@ -5,8 +5,11 @@
 const p = new Promise(function(resolve, reject){
     //kick off some async work.
     //...
+    setTimeout(() => {
+        resolve(1);
+    }, 2000);
     //if successful, then we send the result to consumer promise
-    resolve(1);
+    
     //else reject(new Error('message));
 
 });
