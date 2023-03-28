f551881785d82c4489a59dff08e6252e1b34524a
4sudheer4 <sudheeranugoju4@gmail.com>
2023-03-28 15:44:30 -0700

    parallel exec race

--- async-demo/promise-api.js
+++ async-demo/promise-api.js
@@ -13,9 +13,17 @@ const p2 = new Promise((resolve, reject) => {
     }, 3000);
 });
 
+const p3 = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+        console.log('async operation 3...');
+        resolve('result from p3');
+    }, 3000);
+});
+
 //here the first result and second result gets displayed after 2 seconds consecutively without waiting for 
 //each other. Both are displayed right after 2 seconds.
-//the result will 
-Promise.all([p1,p2])
+//Promise.call(): the result will be appended in an array and display all the results in result after execution of all promises are done. 
+//Promise.race(): only the first executed promise results will be displayed.
+Promise.race([p1,p2,p3])
     .then((result) => {console.log(result)})
     .catch((err) => {console.log(err)}); //fails when atleast one of the promises fail.
