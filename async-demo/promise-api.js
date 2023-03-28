45fe96007ad858149ccf4ac345da49f2a12b53b5
4sudheer4 <sudheeranugoju4@gmail.com>
2023-03-28 13:59:25 -0700

    parallel exec

--- async-demo/promise-api.js
+++ async-demo/promise-api.js
@@ -1,7 +1,8 @@
-const p1 = new Promise((resolve, reject) => {
+const p1 = new Promise((resolve, rejec) => {
     setTimeout(()=>{
         console.log('async operation 1...');
-        resolve(1);
+        // resolve(1);
+        rejec('Error because something failed');
     }, 2000);
 });
 
@@ -9,8 +10,11 @@ const p2 = new Promise((resolve, reject) => {
     setTimeout(()=>{
         console.log('async operation 2...');
         resolve(2);
-    }, 2000);
+    }, 3000);
 });
 
+//here the first result and second result gets displayed after 2 seconds consecutively without waiting for 
+//each other. Both are displayed right after 2 seconds.
 Promise.all([p1,p2])
-    .then((result) => {console.log(result)});
+    .then((result) => {console.log(result)})
+    .catch((err) => {console.log(err)}); //fails when atleast one of the promises fail.
