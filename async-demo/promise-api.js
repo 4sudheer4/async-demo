50a151ba86714ab7d201570b1174ae49bb9569b9
4sudheer4 <sudheeranugoju4@gmail.com>
2023-03-28 13:11:58 -0700

    promise parallel execution

--- async-demo/promise-api.js
+++ async-demo/promise-api.js
@@ -1,4 +1,16 @@
-const p = Promise.resolve({id:1});
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+        console.log('async operation 1...');
+        resolve(1);
+    }, 2000);
+});
 
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+        console.log('async operation 2...');
+        resolve(2);
+    }, 2000);
+});
 
-p.then((result) => console.log(result));
+Promise.all([p1,p2])
+    .then((result) => {console.log(result)});
